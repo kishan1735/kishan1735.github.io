a3255249840042c87ed99ae609ab38ca8f799d7e
kishan1735 <gswwarriorsnba@gmail.com>
2025-01-25 15:31:31 +0530

    fix: change vite config for prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/kishan1735.github.io",
+  base: "/kishan1735.github.io/dist/",
 });
